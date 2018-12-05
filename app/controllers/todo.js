57f7f68664aab825bdb45515b30ef0466037f169
Volodymyr Smal-Stotskiy <volodymyr.smalstotskiy@teamvoy.com>
2018-12-05 17:15:49 +0200

    function to remove an instance of the Task class

--- app/controllers/todo.js
+++ app/controllers/todo.js
@@ -4,7 +4,10 @@ import EmberObject from '@ember/object';
 const list = EmberObject.extend({}).create({});
 
 const Task = EmberObject.extend({
-
+      deleteTask(id) {
+        list.set(id, undefined);
+        delete list[id];
+      }
 });
 
 export default Controller.extend({
