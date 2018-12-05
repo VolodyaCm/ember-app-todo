f6814ade714c765765f046698c8d233b13418900
Volodymyr Smal-Stotskiy <volodymyr.smalstotskiy@teamvoy.com>
2018-12-05 17:16:55 +0200

    new event handler - deleteTask

--- app/components/task-list.js
+++ app/components/task-list.js
@@ -5,6 +5,13 @@ export default Component.extend({
     list: computed('params.[]', function(){
         return this.params[0];
     }),
+    actions: {
+        deleteTask(arg, id) {
+            if(confirm('Delete this task?')) {
+                arg(id);
+            };
+        }
+    }
 }).reopenClass({
     positionalParams: 'params',
 });
