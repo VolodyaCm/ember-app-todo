be0954f83fb1223638f1032d16b7a440210e105e
Volodymyr Smal-Stotskiy <volodymyr.smalstotskiy@teamvoy.com>
2018-12-05 13:39:27 +0200

    import event handler from todo controller

--- app/components/add-task.js
+++ app/components/add-task.js
@@ -1,4 +1,10 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+    addtask: computed('params.[]', function(){
+        return this.params[0];
+      })
+    }).reopenClass({
+      positionalParams: 'params'
 });
