c7dea2a6bbd7d711268347ead0ea4dfd7606a064
Volodymyr Smal-Stotskiy <volodymyr.smalstotskiy@teamvoy.com>
2018-12-05 16:02:10 +0200

    iterative on the task list

--- app/components/task-list.js
+++ app/components/task-list.js
@@ -1,4 +1,10 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+    list: computed('params.[]', function(){
+        return this.params[0];
+    }),
+}).reopenClass({
+    positionalParams: 'params',
 });
