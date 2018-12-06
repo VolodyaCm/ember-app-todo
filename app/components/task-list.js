aeddd0a38f18d6a98083733e22b4c48da7afd9ac
Volodymyr Smal-Stotskiy <volodymyr.smalstotskiy@teamvoy.com>
2018-12-06 12:36:19 +0200

    import functions from todo controller

--- app/components/task-list.js
+++ app/components/task-list.js
@@ -5,11 +5,29 @@ export default Component.extend({
     list: computed('params.[]', function(){
         return this.params[0];
     }),
+    changeId: computed('params.[]', function(){
+        return this.params[1];
+    }),
+    changeActive: computed('params.[]', function(){
+        return this.params[2];
+    }),
+    changePassive: computed('params.[]', function(){
+        return this.params[3];
+    }),
     actions: {
         deleteTask(arg, id) {
             if(confirm('Delete this task?')) {
                 arg(id);
+                this.changeId();
+                this.changeActive();
+                this.changePassive();
             };
+        },
+        completed(changeStatus, id) {
+            console.log(id);
+            changeStatus(id);
+            this.changeActive(id);
+            this.changePassive(id);
         }
     }
 }).reopenClass({
