ae751f6b9e78d13fd8ae5313e2466070f5940ac1
Volodymyr Smal-Stotskiy <volodymyr.smalstotskiy@teamvoy.com>
2018-12-11 11:44:26 +0200

    new action - addtask

--- app/components/add-task.js
+++ app/components/add-task.js
@@ -4,7 +4,12 @@ import { computed } from '@ember/object';
 export default Component.extend({
     addtask: computed('params.[]', function(){
         return this.params[0];
-      })
+    }),
+    actions: {
+      addtask() {
+        this.addtask();
+      },
+    },
     }).reopenClass({
       positionalParams: 'params'
 });
