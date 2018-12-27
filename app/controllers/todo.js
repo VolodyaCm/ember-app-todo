ae6a2329948f4a82e526ea862c9c52d252143aa0
Volodymyr Smal-Stotskiy <volodymyr.smalstotskiy@teamvoy.com>
2018-12-27 13:35:05 +0200

    Fix generate id for task

--- app/controllers/todo.js
+++ app/controllers/todo.js
@@ -1,27 +1,34 @@
 import Controller from '@ember/controller';
 import EmberObject from '@ember/object';
 
-const list = EmberObject.extend({}).create({});
+const list = EmberObject.create({});
+
+function random() {
+    return Math.floor(Math.random() * 10**10);
+}
 
 const Task = EmberObject.extend({
       deleteTask(id) {
         list.set(id, undefined);
         delete list[id];
       }
-});
+}).reopenClass({
+    generateId() {
+        return `t_${random()}`;
+    }
+})
 
 export default Controller.extend({
     id: 0,
     task: '',
     list: list,
     actions: {
-        addtask() {
-            const _id = this.id++;
+        addTask() {
+            const _id = Task.generateId();
             list.set(_id, Task.create({
                 completed: false,
                 task: this.task,
             }));
-            console.log(list);
         }
     }
 
